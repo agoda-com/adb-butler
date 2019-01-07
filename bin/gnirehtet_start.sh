7ae3d9d4cd81c39c2f5ed0b2dfd1b96ae52e5961
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2019-01-07 19:53:54 +0700

    gnirehtet_start.sh should not stop

--- bin/gnirehtet_start.sh
+++ bin/gnirehtet_start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if [ "$GNIREHTET_ENABLED" = "true" ]; then
+if [ "$GNIREHTET_ENABLED" == "true" ]; then
   echo "Starting gnirehtet daemon"
   (cd /; exec ./gnirehtet relay) || return
+else
+  echo "gnirehtet daemon disabled"
+  tail -f /dev/null
 fi
