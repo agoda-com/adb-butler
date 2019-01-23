947e71fc46efb2a2b3fa886b81b648bac8beab30
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2019-01-23 17:49:57 +0700

    improve timeouts

--- bin/gnirehtet_reconnect.sh
+++ bin/gnirehtet_reconnect.sh
@@ -5,8 +5,9 @@ while true; do
   if [ "$GNIREHTET_ENABLED" = "true" ]; then
     adb devices | egrep 'e$' | awk '{ print $1 }' | while read d; do
   	  echo $d
-  	  echo -n | timeout -t 30 adb -s $d reverse localabstract:gnirehtet tcp:31416
-  	  echo -n | timeout -t 30 adb -s $d shell am broadcast -a com.genymobile.gnirehtet.START -n com.genymobile.gnirehtet/.GnirehtetControlReceiver --esa dnsServers 10.120.1.123
+  	  echo -n | timeout -t 5 adb -s $d reverse localabstract:gnirehtet tcp:31416
+  	  echo -n | timeout -t 10 adb -s $d shell am broadcast -a com.genymobile.gnirehtet.START -n com.genymobile.gnirehtet/.GnirehtetControlReceiver --esa dnsServers 10.120.1.123 &
     done
+    wait
   fi
 done
