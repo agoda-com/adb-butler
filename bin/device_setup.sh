df40977942b516fb02f80540e547317e8615306d
Mishra, MritunjayKumar (Agoda) <mritunjaykumar.mishra@agoda.com>
2020-06-30 17:22:49 +0700

    Removed conditional

--- bin/device_setup.sh
+++ bin/device_setup.sh
@@ -49,14 +49,12 @@ function setup_emulator {
     local SERIAL
     SERIAL=`hostname`
     timeout -t 30 adb -s $DEVICE shell su root setprop marathon.serialno $SERIAL
+    timeout -t 30 adb -s $DEVICE shell su root pm disable org.chromium.webview_shell
   fi
 
-  if [[ ! -z "$MARATHON_SERIAL"  ]]; then
-    timeout -t 30 adb -s $DEVICE shell su root pm disable org.chromium.webview_shell
-    timeout -t 30 adb -s $DEVICE shell su root settings put secure spell_checker_enabled 0
-    if [ ! -z "$EMULATOR_TIMEZONE" ]; then
-      timeout -t 30 adb -s $DEVICE shell su root setprop persist.sys.timezone "$EMULATOR_TIMEZONE"
-    fi
+  timeout -t 30 adb -s $DEVICE shell su root settings put secure spell_checker_enabled 0
+  if [ ! -z "$EMULATOR_TIMEZONE" ]; then
+    timeout -t 30 adb -s $DEVICE shell su root setprop persist.sys.timezone "$EMULATOR_TIMEZONE"
   fi
 }
 
